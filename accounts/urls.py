73580e9cf8c86d51ea7b0fcde5b59d55884f33c5
richie <richiepagard@gmail.com>
2025-08-19 13:02:13 +0330

    refactor: Add `swp` files to the `gitignore` and change the url names in the accounts.

--- accounts/urls.py
+++ accounts/urls.py
@@ -15,13 +15,13 @@ app_name = 'accounts'
 
 jwt_urlpatterns = [
     # Generate access and refresh tokens for user login
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/', TokenObtainPairView.as_view(), name='token-obtain-pair'),
     # Refresh access token using a valid refresh token
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
 ]
 auth_urlpatterns = [
-    path('register/', UserRegisterView.as_view(), name='user_register'),
-    path('login/', UserLoginView.as_view(), name='user_login'),
-    path('logout/', UserLogoutView.as_view(), name='user_logout')
+    path('register/', UserRegisterView.as_view(), name='user-register'),
+    path('login/', UserLoginView.as_view(), name='user-login'),
+    path('logout/', UserLogoutView.as_view(), name='user-logout')
 ]
 urlpatterns = jwt_urlpatterns + auth_urlpatterns
