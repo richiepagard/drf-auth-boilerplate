e251c4713d2b663130a1998eb190099378d15f4b
richie <richiepagard@gmail.com>
2025-12-17 22:37:57 +0330

    refactor: Change `models` to a module in `accounts.
    - Create a new directory inside the `accounts` to manage models in the `accounts` app and make the models more organized by modularity.
    
    - Initial the `User` model in the `accounts` app.

--- accounts/models/user_model.py
+++ accounts/models/user_model.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-from .managers import UserManager
+from accounts.managers import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin):
