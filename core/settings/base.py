a9077379bcb2b869007de11331e6bf2f0661ae00
richie <richiepagard@gmail.com>
2025-12-31 22:03:10 +0330

    feat; Config postgresql database as the default database.

--- core/settings/base.py
+++ core/settings/base.py
@@ -68,14 +68,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+from .db_conf import DATABASES
 
 
 # Password validation
