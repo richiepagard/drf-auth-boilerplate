ed1abfdaf22a800f85e2c4fc608997d34ee80e3a
richie <richiepagard@gmail.com>
2026-02-22 23:50:58 +0330

    feat; Add `drf-spectacular` URLs in core URLs list.
    - Set `api/schema/` as the patterns name(base route) of the `drf-spectacular`.
    
    - Add `Swagger` and `Redoc` to the list of `schema` URLs.

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,32 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView
+)
+
+SPECTACULAR_URLS = [
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # UI
+    path(
+        'api/schema/swagger-ui/',
+        SpectacularSwaggerView.as_view(url_name='schema'),
+        name='swagger-ui'
+    ),
+    path(
+        'api/schema/redoc/',
+        SpectacularRedocView.as_view(url_name='schema'),
+        name='redoc'
+    ),
+]
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/auth/', include('accounts.urls', namespace='accounts')),  # JWT & future login/register APIs
+
+    # JWT & future login/register APIs
+    path('api/auth/', include('accounts.urls', namespace='accounts')),
 ]
+
+urlpatterns += SPECTACULAR_URLS
