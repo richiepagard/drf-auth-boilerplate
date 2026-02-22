1896d0dba01757f4e9aac58313a2dd209eaa2002
richie <richiepagard@gmail.com>
2026-02-23 00:15:31 +0330

    feat; Customize `swagger-ui` in `drf-spectacular` configs.

--- core/settings/spectacular_conf.py
+++ core/settings/spectacular_conf.py
@@ -1,6 +1,25 @@
 SPECTACULAR_SETTINGS = {
     'TITLE': 'Django REST Framework Auth Boilerplate',
     'DESCRIPTION': 'A boilerplate for authentication in Django Rest Framework with JWT support.',
+    # Version configs
     'VERSION': '1.7.4',
+    'SCHEMA_PATH_PREFIX': r'/api/v[0-9]',
+
     'SERVE_INCLUDE_SCHEMA': False,
+    # Contact options
+    'CONTACT': {
+        'name': 'Richie Pagard',
+        'email': 'richiepagard@gmail.com',
+        'url': 'https://github.com/richiepagard'
+    },
+
+    # Customize Swagger UI
+    "SWAGGER_UI_SETTINGS":
+    """{
+        deepLinking: true,
+        displayOperationId: true,
+        persistAuthorization: true,
+        presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+        layout: "StandaloneLayout",
+    }""",
 }
