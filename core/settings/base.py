f49f3437b961881c55eb7389d3ba64c6a39da7a0
richie <richiepagard@gmail.com>
2026-02-22 23:26:47 +0330

    feat; Add `drf-spectacular` as the `auto-schema` in DRF Configs.

--- core/settings/base.py
+++ core/settings/base.py
@@ -119,9 +119,12 @@ AUTH_USER_MODEL = 'accounts.User'
 
 # Rest framework configs
 REST_FRAMEWORK = {
+    # Default authentication
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ]
+    ],
+    # Auto schema
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
 
